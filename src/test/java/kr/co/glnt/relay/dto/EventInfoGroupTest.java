846885d680295478bbebb8065ee14e5bb8233a22
glntseongsik <ss.jo@glnt.co.kr>
2021-01-11 14:50:25 +0900

    uuid 로직 변경 System.currentTimMilis -> LocalDateTime.now()

--- src/test/java/kr/co/glnt/relay/dto/EventInfoGroupTest.java
+++ src/test/java/kr/co/glnt/relay/dto/EventInfoGroupTest.java
@@ -1,4 +1,19 @@
+package kr.co.glnt.relay.dto;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.*;
-class EventInfoGroupTest {
-  
+
+public class EventInfoGroupTest {
+
+    @Test
+    public void createUUID() {
+        UUID uuid = UUID.randomUUID();
+        StringBuilder builder = new StringBuilder(uuid.toString());
+        builder.append(LocalDateTime.now());
+        System.out.println("uuid: " + builder.toString());
+    }
 }
