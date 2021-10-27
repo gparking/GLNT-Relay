e60775384a0b0584bd6be7a188cc98182a689a2d
lucydevelop <44827763+lucydevlop@users.noreply.github.com>
2021-10-27 19:46:09 +0900

    Update GlntFolderWatcher.java
    차량 이미지 파일 non empty check

--- src/main/java/kr/co/glnt/relay/watcher/GlntFolderWatcher.java
+++ src/main/java/kr/co/glnt/relay/watcher/GlntFolderWatcher.java
@@ -8,9 +8,14 @@ import kr.co.glnt.relay.service.BreakerFactory;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * 디렉토리 모니터링 클래스
@@ -22,6 +27,12 @@ public class GlntFolderWatcher implements Runnable {
     private WatchService service;
     private FacilityInfo facilityInfo;
     private Map<String, FacilityInfo> gateLprGroup;
+
+    private static final Pattern JPEG_FILE_EXTENSIONS = Pattern.compile("^.+\\.(?i)(?:jpe?g)$");
+    /** Empty file initial retry, in ms. */
+    private static final long INITIAL_RETRY = 50L;
+    /** Empty file maximum retry, in ms. */
+    private static final long MAXIMUM_RETRY = 2000L;
 
     @SneakyThrows
     public GlntFolderWatcher(FacilityInfo facilityInfo, Map<String, FacilityInfo> gateLprGroup) {
@@ -51,8 +62,11 @@ public class GlntFolderWatcher implements Runnable {
                     continue;
                 }
 
+                //파일 정합성 check
                 String fullPath = getFullPath(event);
-                log.info(">>>> {}({}) 파일 생성: {}, size: {} bytes", facilityInfo.getFname(), facilityInfo.getDtFacilitiesId(), fullPath);
+                if (!isValidationImageFile(Paths.get(fullPath))) continue;
+
+                log.info(">>>> {}({}) 파일 생성: {}, size: {} bytes", facilityInfo.getFname(), facilityInfo.getDtFacilitiesId(), fullPath, fullPath.length());
                 switch (BreakerFactory.valueOf(facilityInfo.generateGateLprType())) {
                     // 보조 LPR 이 달려있을 경우 동시에 사진이 들어와
                     // 이벤트 발생 시간을 정확하게 체크하기 위해 신규 스레드 생성.
@@ -101,5 +115,78 @@ public class GlntFolderWatcher implements Runnable {
                 .append("\\")
                 .append(fileName)
                 .toString();
+    }
+
+    private boolean isValidationImageFile(final Path path) {
+        Matcher matcher = JPEG_FILE_EXTENSIONS.matcher(path.toString());
+        if (matcher.matches()) {
+            try
+            {
+                // block until file is non-empty
+                //waitForNonEmptyFile(path);
+
+                for (long retry = INITIAL_RETRY; retry < MAXIMUM_RETRY; retry *= 2)
+                {
+                    if (isNonEmpty(path))
+                    {
+                        return true;
+                    }
+                    try
+                    {
+                        log.trace("  waiting " + retry + "ms for empty file...");
+                        Thread.currentThread().sleep(retry);
+                    }
+                    catch (InterruptedException e)
+                    {
+                        // ok
+                    }
+                }
+                throw new IOException("path " + path + " is empty");
+            }
+            catch (IOException e)
+            {
+                log.warn("unable to create file, " + e.getMessage());
+
+                try
+                {
+                    // delete problematic file from watch directory
+                    log.info("deleting " + path);
+                    Files.delete(path);
+                }
+                catch (IOException ioe)
+                {
+                    // ignore
+                }
+                return false;
+            }
+        } else
+            return false;
+    }
+
+    void waitForNonEmptyFile(final Path path) throws IOException
+    {
+        for (long retry = INITIAL_RETRY; retry < MAXIMUM_RETRY; retry *= 2)
+        {
+            if (isNonEmpty(path))
+            {
+                return;
+            }
+            try
+            {
+                log.trace("  waiting " + retry + "ms for empty file...");
+                Thread.currentThread().sleep(retry);
+            }
+            catch (InterruptedException e)
+            {
+                // ok
+            }
+        }
+        throw new IOException("path " + path + " is empty");
+    }
+
+    static boolean isNonEmpty(final Path path) throws IOException
+    {
+        BasicFileAttributes attr = Files.readAttributes(path, BasicFileAttributes.class);
+        return attr.isRegularFile() && (attr.size() > 0L);
     }
 }
