e530d5da7f01cff7ff5e77996940d5d5a8221d1a
Lucy <>
2021-11-29 09:33:23 +0900

    FOT version

--- src/main/java/kr/co/glnt/relay/service/PaymentService.java
+++ src/main/java/kr/co/glnt/relay/service/PaymentService.java
@@ -32,6 +32,8 @@ public class PaymentService{
     public void paymentProcessing(FacilityInfo facilityInfo, String message) {
         Map<String, Object> receiveData = objectMapper.readValue(message, new TypeReference<Map<String, Object>>() {});
         String type = Objects.toString(receiveData.get("type"), "");
+        Map<String, String> contents = objectMapper.convertValue(receiveData.get("contents"), new TypeReference<Map<String, String>>(){});
+        String responseId = Objects.toString(receiveData.get("responseId"), "");
 
         switch (type) {
             case "vehicleListSearch": // 차량 목록 조회
@@ -49,14 +51,13 @@ public class PaymentService{
                 break;
             case "healthCheck":
                 log.info(">>>> 정산기({}) 헬스체크 메세지 응답 수신: {}", facilityInfo.getDtFacilitiesId(), message);
-                Map<String, String> contents = objectMapper.convertValue(receiveData.get("contents"), new TypeReference<Map<String, String>>(){});
-
                 // 정산기 연결 상태
                 String payStationStatus = Objects.toString(contents.get("paymentFailure"), "");
 
                 // 카드 리더기 연결 상태
                 String cardReaderStatus = Objects.toString(contents.get("icCardReaderFailure"), "");
-                String responseId = Objects.toString(receiveData.get("responseId"), "");
+
+//                responseId = Objects.toString(receiveData.get("responseId"), "");
 
                 gpmsAPI.sendPaymentHealth(FacilityPayloadWrapper.healthCheckPayload(
                         Arrays.asList(
@@ -65,7 +66,38 @@ public class PaymentService{
                         )
                 ));
                 break;
+            case "printerCheck":
+                log.info(">>>> 정산기({}) 프린터체크 메세지 응답 프신: {}", facilityInfo.getDtFacilitiesId(), message);
+//                contents = objectMapper.convertValue(receiveData.get("contents"), new TypeReference<Map<String, String>>(){});
+
+                // 프린터 연결 상태
+                String printFailure = Objects.toString(contents.get("printFailure"), "");
+
+                // 프린터 용지 lack 상태
+                String printPaperLack = Objects.toString(contents.get("printPaperLack"), "");
+
+                // 프린터 문열림 상태
+                String noPrintPaper = Objects.toString(contents.get("noPrintPaper"), "");
+
+                // 카드 리더기 연결 상태
+                String opendoor = Objects.toString(contents.get("opendoor"), "");
+
+                gpmsAPI.sendPaymentHealth(FacilityPayloadWrapper.healthCheckPayload(
+                        Arrays.asList(
+                                FacilityStatus.printStatus(facilityInfo.getDtFacilitiesId(), printFailure),
+                                FacilityStatus.printPaperLackStatus(facilityInfo.getDtFacilitiesId(), printPaperLack),
+                                FacilityStatus.printOpenDoorStatus(facilityInfo.getDtFacilitiesId(), opendoor),
+                                FacilityStatus.noPrintPaperStatus(facilityInfo.getDtFacilitiesId(), noPrintPaper, responseId)
+                        )
+                ));
+                break;
             case "adjustmentdataRequest":
+                log.info(">>>> 정산기({}) 바코드 할인 메세지 응답 수신: {}", facilityInfo.getDtFacilitiesId(), message);
+                //String requestData = objectMapper.writeValueAsString(receiveData);
+                gpmsAPI.sendAdjustmentDataRequest(facilityInfo.getDtFacilitiesId(),message);
+                break;
+
+            case "discountRequest":
                 log.info(">>>> 정산기({}) 바코드 할인 메세지 응답 수신: {}", facilityInfo.getDtFacilitiesId(), message);
                 //String requestData = objectMapper.writeValueAsString(receiveData);
                 gpmsAPI.sendAdjustmentDataRequest(facilityInfo.getDtFacilitiesId(),message);
